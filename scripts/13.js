a3253ecbdcd554998e27eabb4dcd6f0e13a9c50f
EUNJIN6131 <hailey6131@gmail.com>
2023-04-10 14:00:18 +0900

    수정

---
+++ scripts/13.js
@@ -0,0 +1,9 @@
+function show() {
+    // alert("안녕하세요");
+
+    document.getElementById("h2ID").innerHTML = "안녕하세요";
+}
+
+document.addEventListener("DOMContentLoaded", function() {});
+    document.getElementById("h2ID").innerHTML = "안녕하세요";
+
